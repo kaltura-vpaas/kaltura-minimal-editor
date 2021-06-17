1f3ded677ba30ebb20dd282e67bd4ac875c0d98f
Hunterpp <hunter.p@h.p>
2021-06-16 23:41:08 -0400

    init

---
+++ routes/index.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var router = express.Router();
+const kaltura = require('kaltura-client');
+var KalturaClientFactory = require('../lib/kalturaClientFactory');
+
+/* GET home page. */
+router.get('/', async function (req, res, next) {
+  try {
+    //you probably want to make an api call with client, see https://developer.kaltura.com/
+    var adminks = await KalturaClientFactory.getKS('', { type: kaltura.enums.SessionType.ADMIN });
+
+    res.render('index', {
+      ks:adminks
+    });
+
+  } catch (e) {
+    res.render('error', { message: e, error: e });
+  }
+});
+
+router.get('/share', function (req, res, next) {
+  try {
+    res.render('share', { 'entryId': req.query.entryId })
+  } catch (e) {
+    res.render('error', { message: e, error: e });
+  }
+});
+
+//allow the UI to poll for whent the video is ready
+router.get('/status', async function (req, res, next) {
+  var adminks = await KalturaClientFactory.getKS('', { type: kaltura.enums.SessionType.ADMIN });
+  var client = await KalturaClientFactory.getClient(adminks);
+  try {
+    kaltura.services.media.get(req.query.entryId)
+      .execute(client)
+      .then(result => {
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify({ ready: result.status == kaltura.enums.EntryStatus.READY }));
+      });
+  } catch (e) {
+    res.render('error', { message: e, error: e });
+  }
+});
+
+
+
+module.exports = router;
